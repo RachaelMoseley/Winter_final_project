124ff0ecfc61053f74db07032f16173ef8ce4a9a
Student <student@Admins-MacBook-Pro.local>
2025-05-08 17:46:32 -0400

    More of my front end UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Button from 'react-bootstrap/Button';
 function App() {
   return (
     <div className="App">
+      <Button style={{ color: 'black', backgroundColor: 'lightblue', fontSize: '48x', position: 'fixed', textAlign: 'center', left: '50px'}}>About me page</Button>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={{ fontSize: '48x', postion: 'fixed', textAlign: 'center', bottom: '50px' }}>
+        Water Quality Analysis
+        </h1>
+        <h5>
+          Welcome!
+        </h5>
+      <div class="button-container">
+        <button style={{ fontSize: '18px', marginRight: '10px'}}>Select Water Waterbody Name</button>
+        <button style={{ fontSize: '18px'}}>Select Analysis Type</button>
+      </div>
+        <Button style={{ color: 'black', backgroundColor: 'lightblue'}}>Submit</Button>
+        
       </header>
+      
     </div>
   );
 }
